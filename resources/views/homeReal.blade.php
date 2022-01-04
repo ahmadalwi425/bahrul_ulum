a3467ef6e728aa1a51f8a68aa63694fe46cedf14
amarizzal <rizal.ammar91@yahoo.com>
2022-01-05 01:59:43 +0700

    nyicil jumbotron

--- resources/views/homeReal.blade.php
+++ resources/views/homeReal.blade.php
@@ -16,18 +16,22 @@
     <!-- Bootstrap CSS -->
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" crossorigin="anonymous">
 
-    {{-- fullPage.ss --}}
+    {{-- pagepilling.css --}}
     <link rel="stylesheet" href="{{asset('css/jquery.pagepiling.css')}}">
+    
+    {{-- skippr.css --}}
+    <link rel="stylesheet" href="{{asset('css/skippr.css')}}">
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
     
     <title>Pondok Pesantren Bahrul Ulum - Homepage</title>
   </head>
   <body>
 
     {{-- Navbar --}}
-    <nav class="fontNavbar navbar navbar-expand-lg navbar-light bg-light">
+    <nav style="z-index: 99; padding:4px" class="fontNavbar navbar navbar-expand-lg navbar-light bg-light">
       <div class="container d-flex justify-content-between">
         <a class="navbar-brand" href="#">
-          <img src="{{asset('storage/img/logo.png')}}" alt="" height="30">    
+          <img src="{{asset('storage/img/logo.png')}}" alt="" height="50">    
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -78,11 +82,36 @@
         <button class="btn btn-primer">Login</button>
       </div>
     </nav>
-
-
     {{-- FullPage --}}
     <div id="pagepiling">
-      <div class="section">Some section</div>
+      <div style="margin-top: -38px" class="section">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="z-index: 99">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="img-fluid" style="max-height: 50%" src="{{asset('storage/img/homepage 1.jpg')}}" class="d-block mx-auto h-1" alt="{{asset('storage/img/homepage.jpg')}}">
+            </div>
+            <div class="carousel-item">
+              <img src="{{asset('storage/img/homepage 2.jpg')}}" class="d-block w-100" alt="{{asset('storage/img/homepage 2.jpg')}}">
+            </div>
+            <div class="carousel-item">
+              <img src="{{asset('storage/img/homepage 3.jpg')}}" class="d-block w-100" alt="{{asset('storage/img/homepage 3.jpg')}}">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
       <div class="section">Some section</div>
       <div class="section">Some section</div>
       <div class="section">Some section</div>
@@ -103,11 +132,18 @@
     <script src="{{asset('js/jquery.js')}}" ></script>
     <!-- fullPage.js -->
     <script src="{{asset('js/jquery.pagepiling.js')}}" ></script>
+    <!-- skippr.js -->
+    <script src="{{asset('js/skippr.js')}}" ></script>
 
     <script>
       $(document).ready(function() {
-	$('#pagepiling').pagepiling();
-});
+        $('#pagepiling').pagepiling({
+          easing: 'linear',
+          animateAnchor: false
+        });
+      });
+      $(document).ready(function(){
+      });    
     </script>
   </body>
 </html>
